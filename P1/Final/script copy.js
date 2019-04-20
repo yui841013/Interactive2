cb21e376211fe3793d01d983d273174fa0b8641d
Stephanie Chou <stephanie.chou@cca.edu>
2019-04-20 11:22:25 -0700

    e

--- P1/Final/script copy.js
+++
@@ -1,7 +0,0 @@
-
-    $(".rectangleleft").mouseenter(function(){
-        $(".rectangleleft").css("background", "red");
-    });
-    $(".rectangleleft").mouseleave(function(){
-        $(".rectangleleft").css("background", "lightgray");
-    });
